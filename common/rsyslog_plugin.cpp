39e5b8c240cb9564bdaf8c4990eb871404a52c94
Ubuntu <zain@zb-dev-vm.022x1jpnpm4u1iy2d325acts3c.yx.internal.cloudapp.net>
2022-04-29 22:06:51 +0000

    Add rsyslog_plugin for POC/demo purposes

---
+++ common/rsyslog_plugin.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <unordered_set>
+#include <regex>
+#include <tuple>
+#include "logger.h"
+#include "json.hpp"
+
+using namespace std;
+using namespace nlohmann;
+
+string parserCfgFile = "{}_parse.rc.json";
+ofstream outfile;
+string logfile = "/tmp/{}_logfile";
+json regexList = json::array();
+string pgmName = "";
+unordered_set<string> errReportedTags;
+
+void loadRegexlist(string rcPath, string pgmName) {
+	// SWSS_LOG_ENTER();
+	fstream rcfile;
+	parserCfgFile = regex_replace(parserCfgFile, regex("\\{}"), pgmName);
+	parserCfgFile = rcPath + parserCfgFile;
+	rcfile.open(parserCfgFile, ios::in);
+	if (!rcfile) {
+		cerr << "No such file: " << parserCfgFile << endl;
+		return;
+	}
+	rcfile >> regexList;
+	// SWSS_LOG_INFO("loardRegexList: file: %s len=%d", parserCfgFile, regexList.size());
+}
+
+tuple<bool, json> matchRegex(string msg) {
+	// SWSS_LOG_ENTER();
+	bool isMatch = false;
+	json structuredEvent;
+	for (int i = 0; i < regexList.size(); i++) {
+		string regexString = regexList[i]["regex"];
+		regex expression(regexString);
+		match_results<string::const_iterator> matchResults;
+		regex_search(msg, matchResults, expression);
+		vector<string> groups;
+		// first match in groups is entire msg string
+		for(int j = 1; j < matchResults.size(); j++) {
+			groups.push_back(matchResults.str(i));
+		}
+		vector<string> params = regexList[i]["params"];
+		string tag = regexList[i]["tag"];
+		structuredEvent["tag"] = tag;
+		structuredEvent["program"] = pgmName;
+		if(params.size() != groups.size() && errReportedTags.find(tag) == errReportedTags.end()) {
+			errReportedTags.insert(tag);
+			// SWSS_LOG_ERROR("%s: params mismatched, params length: %d group length: %d for message:%s", pgmName, params.size(), groups.size(), msg);
+			structuredEvent["parsed"] = groups;
+		} else {
+			for(int j = 0; j < params.size(); j++) {
+				structuredEvent[params[j]] = "groups[j]";
+			}
+			isMatch = true;
+			break;
+		}
+	}
+	return make_tuple(isMatch, structuredEvent);
+}
+
+void onInit(bool isDebug, string rcPath, string pgmName) {
+	//swss::Logger::getInstance().setMinPrio(swss::Logger::SWSS_NOTICE);
+	//SWSS_LOG_ENTER();
+	// SWSS_LOG_DEBUG("onInit called");
+	loadRegexlist(rcPath, pgmName);
+	if (isDebug) {
+		logfile = regex_replace(logfile, regex("\\{}"), pgmName);
+		outfile.open(logfile);
+	}
+}
+
+void onMessage(string msg) {
+	// SWSS_LOG_ENTER();
+	tuple<bool, json> regexMatch = matchRegex(msg);
+	bool match = get<0>(regexMatch);
+	json event = get<1>(regexMatch);
+
+	if(outfile) {
+		outfile << "match: " << match << "\noriginal message: " << msg << "\nstructured event: " << event << endl;
+	}
+
+	if(match) {
+		// call event api with event
+	}
+}
+
+void onExit() {
+	// SWSS_LOG_ENTER();
+	// SWSS_LOG_DEBUG("onExit called");
+	if(outfile) {
+		outfile.close();
+	}
+}
+
+void run() {
+	// SWSS_LOG_ENTER();
+	cout << "OK" << endl;
+	
+	while(true) {
+		string line;
+		getline(cin, line);
+		if(line.empty()) {
+			continue;
+		}
+		onMessage(line);
+		cout << "Called onMessage on: " << line << endl;
+	}
+}
+
+void showUsage(string name) {
+	cerr << "Usage: " << name << " <option(s)> SOURCES\n"
+		<< "Options:\n"
+		<< "\t-h,--help\t\tShow this help message\n"
+		<< "\t-d,--debug,type=bool\t\tRun with debug log level"
+		<< "\t-p,--pgm-name,type=string,required=True\t\tName of the program dumping the message"
+		<< "\t-r,--rc-path\t\tDirectory of the rc file"
+		<< endl;
+}
+
+int main(int argc, char** argv) {
+	// SWSS_LOG_ENTER();
+	json j;
+
+	if (argc < 2) { // required param is not provided
+		showUsage(argv[0]);
+		return 1;
+	}
+
+	bool isDebug = false;
+	string programName = "";
+	string rcPath = "/etc/rsyslog.d/";
+
+	for(int i = 1; i < argc; i++) {
+		string arg = argv[i];
+		if (arg == "-h" || arg == "--help") {
+			showUsage(argv[0]);
+			return 0;
+		} else { // no argument for flag
+			if (i + 1 >= argc) {
+				cout << "Missing argument after flag" << endl;
+				showUsage(argv[0]);
+				return 1;
+			}
+		}
+		if (arg == "-d" || arg == "--debug") {
+			string debugFlag = argv[++i];
+			if (debugFlag != "true" && debugFlag != "false") { // invalid argument for debugFlag
+				showUsage(argv[0]);
+				return 1;
+			}
+			isDebug = (debugFlag == "true") ? true : false;
+		} else if (arg == "-p" || arg == "--pgm-name") {
+			programName = argv[++i];
+		} else if (arg == "-r" || arg == "--rc-path") {
+			rcPath = argv[++i];
+		} else { // invalid flag
+			showUsage(argv[0]);
+			return 1;
+		}
+	}
+	pgmName = programName;
+	onInit(isDebug, rcPath, programName);
+	run();
+
+	return 0;
+}
